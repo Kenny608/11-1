48d44b0eaafd48966192f735ad273d5e7eed3744
Kenny <kennypluchau@gmail.com>
2023-11-01 15:25:35 -0700

    Initial

---
+++ Data-persistence/Data_persistenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_persistenceApp.swift
+//  Data-persistence
+//
+//  Created by Kenny Luchau on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Data_persistenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Data-persistenceUITests/Data_persistenceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Data_persistenceUITestsLaunchTests.swift
+//  Data-persistenceUITests
+//
+//  Created by Kenny Luchau on 11/1/23.
+//
+
+import XCTest
+
+final class Data_persistenceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
